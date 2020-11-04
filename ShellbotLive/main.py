fc5041dd021b603b5f2fc6b9f8d10c3010515ce8
Dominic Horn <dominic1123@live.co.uk>
2020-11-04 20:41:37 +0000

    Added colour filter and threshold

--- ShellbotLive/main.py
+++ ShellbotLive/main.py
@@ -15,11 +15,30 @@ class ShellbotLive():
     def __init__(self):
         while True:
             screen = np.array(ImageGrab.grab(bbox = (0,40,1920,927)))
-            cv2.imshow('Screen', screen)
+            player_filtered_screen = self.colour_filter(screen, self.lower_green, self.upper_green)
+            player_filtered_screen = self.threshold(player_filtered_screen)
+            cv2.imshow('Green', player_filtered_screen)
 
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 cv2.destroyAllWindows()
                 exit()
+
+    def colour_filter(self, screen, lower_colour, upper_colour):
+        hsv = cv2.cvtColor(screen, cv2.COLOR_BGR2HSV)
+
+        mask = cv2.inRange(hsv, lower_colour, upper_colour)
+        res = cv2.bitwise_and(screen, screen, mask=mask)
+
+        return res
+
+    def threshold(self, screen):
+        blurred_screen = cv2.GaussianBlur(screen, (11, 11), 0)
+
+        thresh_screen = cv2.threshold(blurred_screen, 100, 255, cv2.THRESH_BINARY)[1]
+        thresh_screen = cv2.erode(thresh_screen, None, iterations=2)
+        thresh_screen = cv2.dilate(thresh_screen, None, iterations=4)
+
+        return thresh_screen
 
 if __name__ == '__main__':
     ShellbotLive()
